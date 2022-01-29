6ecb4246e16f7dc3cba552db2b10a604e91a3496
RafaeLovera6 <rafaelovera6@gmail.com>
2022-01-29 01:57:25 +0000

    finished

--- src/models.py
+++ src/models.py
@@ -8,23 +8,48 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    
 
-class Address(Base):
-    __tablename__ = 'address'
+class Favorite(Base):
+    __tablename__ = 'favorite'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    character_id = Column(Integer, ForeignKey('characters.id'))
+
+class Character(Base):
+    __tablename__ = 'characters'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    height = Column(Integer, nullable=False)
+    hair_color = Column(Integer, nullable=False)
+    eye_color = Column(String, nullable=False)
+    birth_year = Column(String, nullable=False)
+    gender = Column(String, nullable=False)
+
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    climate = Column(String, nullable=False)
+    rotation_period = Column(Integer,nullable=False)
+    orbital_period = Column(Integer, nullable=False)
+    terrain = Column(String, nullable=False)
+    population = Column(Integer, nullable=False)
+
+
+
 
     def to_dict(self):
         return {}
